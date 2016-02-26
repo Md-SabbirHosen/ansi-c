da106e8446fba847f244e45a36b2e87531937e2b
Reez Patel <reezpatel@gmail.com>
2016-02-26 19:00:48 +0530

    Ch-6 Completed

---
+++ ch-6/6-1.c
@@ -0,0 +1,21 @@
+/*********************************************************
+*		REVERSE NO.				 *
+*	Author: Reez Patel				 *
+*	Source: Ex: 6.1 (P-188) = ANSI-C		 *
+**********************************************************/
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	printf("Enter A Number: ");
+	scanf("%d",&n);
+	
+	while(n > 0)
+	{
+		printf("%d",n%10);
+		n /= 10;
+	}
+	printf("\n");
+	return 0;
+}
---
+++ ch-6/6-5.c
@@ -0,0 +1,33 @@
+/*********************************************************
+*		FIBONACCI SERIES			 *
+*	Author: Reez Patel				 *
+*	Source: Ex: 6.5 (P-188) = ANSI-C		 *
+**********************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a=1,b=1,n,temp;
+	printf("Enter A Number: ");
+	scanf("%d",&n);
+	if(n <= 0)
+		exit(0);
+	if(n == 1)
+	{
+		printf("%d ... \n",a);
+		exit(0);
+	}
+	n -= 2;	
+	printf("%d %d ",a,b);
+	while(n-- > 0)
+	{
+		printf("%d ",a+b);
+		temp = a;
+		a = b;
+		b += temp;
+	}
+
+	printf("... \n");
+
+	return 0;
+}
